320ad17a6dfa7712c2ddbd390bf997558a1823ae
Teemu Pöytäniemi <teemu.poytaniemi@student.tut.fi>
2023-05-25 20:35:34 +0300

    Added new info component

---
+++ adminUI/src/components/Dialogs/InfoDialog/InfoDialog.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './InfoDialog.scss';
+import Dialog from '../Dialog/Dialog';
+import Button from '../../Button/Button';
+
+const DeleteDialog = (props) => {
+  const deleteButtonStyles = {
+    backgroundColor: '#607d8b'
+  }
+  return (
+    <Dialog>
+      <div className='info-dialog flex-column-center'>
+        <h2 className='info-dialog__header'>{props.header}</h2>
+        <div className='info-dialog__main-content'>
+          {props.content()}
+        </div>
+        <div className='info-dialog__button-container flex-row'>
+          <Button onClick={props.close} style={deleteButtonStyles}>Peruuta</Button>
+          <Button onClick={props.accept}>{props.acceptButtonText}</Button>
+
+        </div>
+      </div>
+    </Dialog>
+    
+  );
+}
+
+export default DeleteDialog;
